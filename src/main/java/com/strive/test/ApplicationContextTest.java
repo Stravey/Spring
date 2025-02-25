8e2d610eb7abc3751a99ca75afed09c61efa1183
strive <2205487298@qq.com>
2025-02-26 00:22:56 +0800

    update

--- src/main/java/com/strive/test/ApplicationContextTest.java
+++ src/main/java/com/strive/test/ApplicationContextTest.java
@@ -5,9 +5,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationContextTest {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserService userService = (UserService) applicationContext.getBean("userService");
         System.out.println(userService);
     }
+
 }
