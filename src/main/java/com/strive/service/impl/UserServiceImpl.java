543dace1494ff351e5cfd6f36232b729f084f739
strive <2205487298@qq.com>
2025-02-26 19:00:28 +0800

    update

--- src/main/java/com/strive/service/impl/UserServiceImpl.java
+++ src/main/java/com/strive/service/impl/UserServiceImpl.java
@@ -5,6 +5,12 @@ import com.strive.service.UserService;
 
 public class UserServiceImpl implements UserService {
 
+    public UserServiceImpl() {
+        System.out.println("UserServiceImpl");
+    }
+
+    private UserDao userDao;
+
     //该方法BeanFactory调用该方法 从BeanFactory中获得userDao设置到此处
     public void setUserDao(UserDao userDao) {
         System.out.println("哈哈userDao哈哈" + userDao);
